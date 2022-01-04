6040f1b7c947aafcb293e1f555fc7ae0887c1c16
jy_lee <qjarlfls@naver.com>
2022-01-04 14:36:34 +0900

    add main

---
+++ src/com/java/s1/method/MethodMain.java
@@ -0,0 +1,13 @@
+package com.java.s1.method;
+
+public class MethodMain {
+
+	public static void main(String[] args) {
+		
+		System.out.println("MainMethod Start");
+		
+		System.out.println("MainMethod Finish");
+
+	}
+
+}
