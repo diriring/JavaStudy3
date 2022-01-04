be9ea984a4912d2d6ac5278718649180c4366fef
jy_lee <qjarlfls@naver.com>
2022-01-04 15:35:42 +0900

    update main

--- src/com/java/s1/method/MethodMain.java
+++ src/com/java/s1/method/MethodMain.java
@@ -6,6 +6,18 @@ public class MethodMain {
 		
 		System.out.println("MainMethod Start");
 		
+		ReturnStudy rs = new ReturnStudy();
+		rs.test1();
+		
+		int count = rs.test2();
+		System.out.println("Main : " + count);
+		
+		String chars = rs.test3();
+		System.out.println(chars);
+		
+		System.out.println(Math.random());
+		
+		
 		System.out.println("MainMethod Finish");
 
 	}
